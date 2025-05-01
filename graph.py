1529da9908ec0a56204797d9dd6c7d5c30d4de5d
abdixaden <72987030+abdixaden@users.noreply.github.com>
2025-05-01 13:14:15 -0400

    Load and Process the Data

--- graph.py
+++ graph.py
@@ -5,3 +5,6 @@ with open('OHRU.csv', 'r') as file:
     header = next(reader)  # Get the header row
     for index, column_name in enumerate(header):
         print(f"Column {index}: {column_name}")
+        
+data = pd.read_csv('OHRU.csv', parse_dates=['DATE'])
+ 
